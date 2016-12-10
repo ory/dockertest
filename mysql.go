00ef5b23316bc63e77c49cde302e5a17ee6a7309
Jason E. Aten <j.e.aten@gmail.com>
2016-12-10 10:40:47 -0600

    SetupMySQLContiner takes command line arguments to run.
    revert the edb9d9444e6868c1b1e279098b628e7b8e6f0714 parseTime dsn addition which breaks things
    
    fixes #71

--- mysql.go
+++ mysql.go
@@ -16,30 +16,34 @@ const (
 
 // SetupMySQLContainer sets up a real MySQL instance for testing purposes,
 // using a Docker container. It returns the container ID and its IP address,
-// or makes the test fail on error.
-func SetupMySQLContainer() (c ContainerID, ip string, port int, err error) {
+// or makes the test fail on error. The commandLineArgs can be nil and
+// will be appended to the docker run command line. For example,
+// commandLineArgs could be used to provide "--binlog-format=ROW";
+// if that was required for testing on mysql.
+//
+func SetupMySQLContainer(commandLineArgs ...string) (c ContainerID, ip string, port int, err error) {
 	port = RandomPort()
 	forward := fmt.Sprintf("%d:%d", port, 3306)
 	if BindDockerToLocalhost != "" {
 		forward = "127.0.0.1:" + forward
 	}
 	c, ip, err = SetupContainer(MySQLImageName, port, 10*time.Second, func() (string, error) {
-		return run("--name", GenerateContainerID(), "-d", "-p", forward, "-e", fmt.Sprintf("MYSQL_ROOT_PASSWORD=%s", MySQLPassword), MySQLImageName)
+		return run(append([]string{"--name", GenerateContainerID(), "-d", "-p", forward, "-e", fmt.Sprintf("MYSQL_ROOT_PASSWORD=%s", MySQLPassword), MySQLImageName}, commandLineArgs...)...)
 	})
 	return
 }
 
 // ConnectToMySQL starts a MySQL image and passes the database url to the connector callback function.
 // The url will match the username:password@tcp(ip:port) pattern (e.g. `root:root@tcp(123.123.123.123:3131)`)
-func ConnectToMySQL(tries int, delay time.Duration, connector func(url string) bool) (c ContainerID, err error) {
-	c, ip, port, err := SetupMySQLContainer()
+func ConnectToMySQL(tries int, delay time.Duration, connector func(url string) bool, commandLineArgs ...string) (c ContainerID, err error) {
+	c, ip, port, err := SetupMySQLContainer(commandLineArgs...)
 	if err != nil {
 		return c, fmt.Errorf("Could not set up MySQL container: %v", err)
 	}
 
 	for try := 0; try <= tries; try++ {
 		time.Sleep(delay)
-		url := fmt.Sprintf("%s:%s@tcp(%s:%d)/mysql?parseTime=true", MySQLUsername, MySQLPassword, ip, port)
+		url := fmt.Sprintf("%s:%s@tcp(%s:%d)/mysql", MySQLUsername, MySQLPassword, ip, port)
 		if connector(url) {
 			return c, nil
 		}
