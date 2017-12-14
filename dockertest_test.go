74cd6113fe6930f7bf24d9ff327e1946fb5335bc
Sylvain Cleymans <sylvain.cleymans@gmail.com>
2017-12-14 14:22:08 +0100

    Add method to build and run an image
    Signed-off-by: Sylvain Cleymans <sylvain.cleymans@gmail.com>

--- dockertest_test.go
+++ dockertest_test.go
@@ -3,15 +3,16 @@ package dockertest
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"testing"
 
+	dc "github.com/fsouza/go-dockerclient"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	dc "github.com/fsouza/go-dockerclient"
 )
 
 var docker = os.Getenv("DOCKER_URL")
@@ -77,12 +78,12 @@ func TestMongo(t *testing.T) {
 func TestContainerWithName(t *testing.T) {
 	resource, err := pool.RunWithOptions(
 		&RunOptions{
-			Name: "db",
+			Name:       "db",
 			Repository: "postgres",
-			Tag: "9.5",
+			Tag:        "9.5",
 		})
 	require.Nil(t, err)
-	assert.Equal(t,"/db", resource.Container.Name)
+	assert.Equal(t, "/db", resource.Container.Name)
 
 	require.Nil(t, pool.Purge(resource))
 }
@@ -91,13 +92,31 @@ func TestContainerWithPortBinding(t *testing.T) {
 	resource, err := pool.RunWithOptions(
 		&RunOptions{
 			Repository: "postgres",
-			Tag: "9.5",
+			Tag:        "9.5",
 			PortBindings: map[dc.Port][]dc.PortBinding{
 				"5432/tcp": {{HostIP: "", HostPort: "5433"}},
 			},
 		})
 	require.Nil(t, err)
-	assert.Equal(t,"5433", resource.GetPort("5432/tcp"))
+	assert.Equal(t, "5433", resource.GetPort("5432/tcp"))
 
+	require.Nil(t, pool.Purge(resource))
+}
+
+func TestBuildImage(t *testing.T) {
+	// Create Dockerfile in temp dir
+	dir, _ := ioutil.TempDir("", "dockertest")
+	defer os.RemoveAll(dir)
+
+	dockerfilePath := dir + "/Dockerfile"
+	ioutil.WriteFile(dockerfilePath,
+		[]byte("FROM postgres:9.5"),
+		0644,
+	)
+
+	resource, err := pool.BuildAndRun("postgres-test", dockerfilePath, nil)
+	require.Nil(t, err)
+
+	assert.Equal(t, "/postgres-test", resource.Container.Name)
 	require.Nil(t, pool.Purge(resource))
 }
