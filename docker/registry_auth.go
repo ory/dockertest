4b8b3b49a37f36192d6b9dc3efce5aaf58b64740
Travis Cline <travis.cline@gmail.com>
2023-03-08 10:25:42 -0800

    chore: add expected header

--- docker/registry_auth.go
+++ docker/registry_auth.go
@@ -1,3 +1,6 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
 // Copyright 2013 go-dockerclient authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
